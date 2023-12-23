383cccf8bf58a946ae3e4ca848f80d8a4b9ec122
Sergio Vilar <sergio@vilar.cc>
2023-12-23 09:49:06 -0300

    Minifica search-form

--- assets/search-form.js
+++ assets/search-form.js
@@ -1,56 +1,71 @@
-var SearchForm = /*@__PURE__*/(function (HTMLElement) {
+"use strict";
+
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+function _wrapNativeSuper(Class) { var _cache = typeof Map === "function" ? new Map() : undefined; _wrapNativeSuper = function _wrapNativeSuper(Class) { if (Class === null || !_isNativeFunction(Class)) return Class; if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() { return _construct(Class, arguments, _getPrototypeOf(this).constructor); } Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _setPrototypeOf(Wrapper, Class); }; return _wrapNativeSuper(Class); }
+function _construct(Parent, args, Class) { if (_isNativeReflectConstruct()) { _construct = Reflect.construct.bind(); } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () { })); return true; } catch (e) { return false; } }
+function _isNativeFunction(fn) { return Function.toString.call(fn).indexOf("[native code]") !== -1; }
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+var SearchForm = /*#__PURE__*/function (_HTMLElement) {
+  _inherits(SearchForm, _HTMLElement);
+  var _super = _createSuper(SearchForm);
   function SearchForm() {
-    var this$1 = this;
-
-    HTMLElement.call(this);
-    this.input = this.querySelector('input[type="search"]');
-    this.resetButton = this.querySelector('button[type="reset"]');
-
-    if (this.input) {
-      this.input.form.addEventListener('reset', this.onFormReset.bind(this));
-      this.input.addEventListener(
-        'input',
-        debounce(function (event) {
-          this$1.onChange(event);
-        }, 300).bind(this)
-      );
+    var _this;
+    _classCallCheck(this, SearchForm);
+    _this = _super.call(this);
+    _this.input = _this.querySelector('input[type="search"]');
+    _this.resetButton = _this.querySelector('button[type="reset"]');
+    if (_this.input) {
+      _this.input.form.addEventListener('reset', _this.onFormReset.bind(_assertThisInitialized(_this)));
+      _this.input.addEventListener('input', debounce(function (event) {
+        _this.onChange(event);
+      }, 300).bind(_assertThisInitialized(_this)));
     }
+    return _this;
   }
-
-  if ( HTMLElement ) SearchForm.__proto__ = HTMLElement;
-  SearchForm.prototype = Object.create( HTMLElement && HTMLElement.prototype );
-  SearchForm.prototype.constructor = SearchForm;
-
-  SearchForm.prototype.toggleResetButton = function toggleResetButton () {
-    var resetIsHidden = this.resetButton.classList.contains('hidden');
-    if (this.input.value.length > 0 && resetIsHidden) {
-      this.resetButton.classList.remove('hidden');
-    } else if (this.input.value.length === 0 && !resetIsHidden) {
-      this.resetButton.classList.add('hidden');
+  _createClass(SearchForm, [{
+    key: "toggleResetButton",
+    value: function toggleResetButton() {
+      var resetIsHidden = this.resetButton.classList.contains('hidden');
+      if (this.input.value.length > 0 && resetIsHidden) {
+        this.resetButton.classList.remove('hidden');
+      } else if (this.input.value.length === 0 && !resetIsHidden) {
+        this.resetButton.classList.add('hidden');
+      }
     }
-  };
-
-  SearchForm.prototype.onChange = function onChange () {
-    this.toggleResetButton();
-  };
-
-  SearchForm.prototype.shouldResetForm = function shouldResetForm () {
-    return !document.querySelector('[aria-selected="true"] a');
-  };
-
-  SearchForm.prototype.onFormReset = function onFormReset (event) {
-    // Prevent default so the form reset doesn't set the value gotten from the url on page load
-    event.preventDefault();
-    // Don't reset if the user has selected an element on the predictive search dropdown
-    if (this.shouldResetForm()) {
-      this.input.value = '';
-      this.input.focus();
+  }, {
+    key: "onChange",
+    value: function onChange() {
       this.toggleResetButton();
     }
-  };
-
+  }, {
+    key: "shouldResetForm",
+    value: function shouldResetForm() {
+      return !document.querySelector('[aria-selected="true"] a');
+    }
+  }, {
+    key: "onFormReset",
+    value: function onFormReset(event) {
+      // Prevent default so the form reset doesn't set the value gotten from the url on page load
+      event.preventDefault();
+      // Don't reset if the user has selected an element on the predictive search dropdown
+      if (this.shouldResetForm()) {
+        this.input.value = '';
+        this.input.focus();
+        this.toggleResetButton();
+      }
+    }
+  }]);
   return SearchForm;
-}(HTMLElement));
-
+}( /*#__PURE__*/_wrapNativeSuper(HTMLElement));
 customElements.define('search-form', SearchForm);
-
